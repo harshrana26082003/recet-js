fdd74515480d5ff7755442d1036e534f10bfd451
harshrana26082003 <112611545+harshrana26082003@users.noreply.github.com>
2023-03-15 12:44:59 +0530

    Add files via upload

---
+++ my new/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from './Navigtion/Navbar'
+import Home from './Header/Header'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+<Navbar></Navbar>
+<Home></Home>
+    </>
+);
+
+
