d3c407acd21883a9d640e0c22a6ed5b0805afbbc
harshrana26082003 <112611545+harshrana26082003@users.noreply.github.com>
2023-06-12 12:51:17 +0530

    Delete module 7 directory

--- module 7/from api/src/appication/Storepath.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from "../fetrue/Userslice";
-
-export default configureStore({
-    reducer:{
-        user : useReducer
-    },
-})
